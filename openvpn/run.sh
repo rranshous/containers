67517f3abd0b1247e719b0c26c0a786d17fb7196
Robby Ranshous <rranshous@gmail.com>
2014-12-07 20:58:41 -0500

    SOCKS5 proxy and openvpn

---
+++ openvpn/run.sh
@@ -0,0 +1,1 @@
+docker run --name openvpnd -d --privileged -p 1194:1194/udp -p 443:443/tcp openvpn
---
+++ openvpn/run_serveconfig.sh
@@ -0,0 +1,1 @@
+docker run -t -i -p 1195:8080 --volumes-from openvpnd openvpn /usr/local/sbin/serveconfig

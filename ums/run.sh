25607d55206e110b8cf442787fd3d749a1126325
Roby Ranshous <rranshous@gmail.com>
2016-01-17 00:10:34 +0100

    WIP: ums for rpi

--- ums/run.sh
+++ ums/run.sh
@@ -1,3 +1,3 @@
 docker kill ums
 docker rm ums
-docker run --net=host --privileged -p 5001:5001 -v /gut/media:/data/media --name ums -d slag.local:4567/ums
+docker run --net=host --privileged -p 5001:5001 -v /downloads:/data/media --name ums -d ums

9bdda33611fe7ea0cca07894de17def874058964
Roby Ranshous <rranshous@gmail.com>
2016-01-17 21:24:30 +0100

    minidlna work, still having issues on restart i think

--- minidlna/run.sh
+++ minidlna/run.sh
@@ -1,1 +1,3 @@
-docker run --net=host -d --name minidlna -v /downloads:/data -p 8200:8200 -p 1900:1900 minidlna
+docker kill minidlna
+docker rm minidlna
+docker run --net=host -d --name minidlna -v /downloads:/data -p 8200:8200 -p 1900:1900 rranshous/minidlna-rpi

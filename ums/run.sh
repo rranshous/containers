ad62ef8e01847e0cab024b2e4a6fe9cfe1f63d3a
Robby Ranshous <rranshous@gmail.com>
2016-08-29 23:19:54 -0400

    typo

--- ums/run.sh
+++ ums/run.sh
@@ -1,1 +1,1 @@
-docker run --restart=always --net=host --privileged -v /downloads:/data/media --name ums-2 -d ums
+docker run --restart=always --net=host --privileged -v /downloads:/data/media --name ums -d ums

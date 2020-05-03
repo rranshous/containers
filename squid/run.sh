2586f316ee76da0e792b8d40fd7dafbdc6e687c2
Robby Ranshous <rranshous@gmail.com>
2020-05-03 14:27:23 -0400

    squid volume options

--- squid/run.sh
+++ squid/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker stop squid
-docker remove squid
-docker run -d --name=squid --restart=always -p 3128:3128 sameersbn/squid
+docker rm squid
+docker run -d --name=squid --restart=always -p 3128:3128 --volume /srv/docker/squid/cache:/var/spool/squid3 sameersbn/squid

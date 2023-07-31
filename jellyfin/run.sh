b7ab220352bdb5b386fa5cfd50d37f3d7fd4f5d6
Robby Ranshous <rranshous@gmail.com>
2023-07-31 19:05:32 -0400

    jellyfin hw acc

--- jellyfin/run.sh
+++ jellyfin/run.sh
@@ -4,6 +4,7 @@ set -x
 docker run -d \
 	--name jellyfin \
 	--net=host \
+	--device /dev/dri/renderD128:/dev/dri/renderD128 \
 	--volume /data/cache/jellyfin:/cache \
 	--volume /data/data/jellyfin:/config \
 	--mount type=bind,source=/data/media,target=/media,readonly \

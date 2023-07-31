01789858428d8711e3ced6f5dc03d96810b29c1f
Robby Ranshous <rranshous@gmail.com>
2023-07-31 19:47:35 -0400

    modernize samba

--- samba/run.sh
+++ samba/run.sh
@@ -1,14 +1,8 @@
 docker kill samba
 docker rm samba
-docker run -d -it \
+docker run -d \
   --name samba \
   -h samba \
-  -p 138:138/udp \
-  -p 139:139 \
   -p 445:445 \
-  -p 445:445/udp \
-  -v /public:/var/www/html \
-  -v /private:/home/open/private \
-  -e SMB_USER='open' \
-  -e SMB_PASS='sesime' \
-  appcontainers/samba
+  -v /data/data/samba:/samba \
+  rranshous/samba
